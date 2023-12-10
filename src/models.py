2faf90bcaf9bd39a13fb120cd1090f1f105d8ac0
Manuel Garay <84086007+uykarma@users.noreply.github.com>
2023-12-10 01:31:16 +0000

    [Actualizacion y subida de archivo] - Se actualiza models.py para que se adecue a la base de datos de star wars - Se sube la captura de QuickDBD del MER perteneciente a star wars - Se sube la captura resultante de ejecutar el models.py

--- src/models.py
+++ src/models.py
@@ -1,29 +1,81 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Float
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class Planet(Base):
+    __tablename__ = 'Planet'
+    PlanetID = Column(Integer, primary_key=True)
+    Name = Column(String(50), unique=True ,nullable=False)
+    Population = Column(Integer)
+    Terrain = Column(String(50))
+    Climate = Column(String(50))
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+class Character(Base):
+    __tablename__ = 'Character'
+    CharacterID = Column(Integer, primary_key=True)
+    Name = Column(String(50), unique=True,nullable=False)
+    Height = Column(Float)
+    Mass = Column(Float)
+    BirthYear = Column(String(10))
+    Homeworld = Column(Integer, ForeignKey(Planet.PlanetID))
+    Planet = relationship(Planet)
+
+class Starship(Base):
+    __tablename__ = 'Starship'
+    StarshipID = Column(Integer, primary_key=True)
+    Name = Column(String(50), unique=True, nullable=False)
+    Model = Column(String(50), nullable=False)
+    Type = Column(String(250))
+    Pilotid = Column(Integer, ForeignKey(Character.CharacterID))
+    C = relationship(Character)
+
+class Vehicle(Base):
+    __tablename__ = 'Vehicle'
+    VehicleID = Column(Integer, primary_key=True)
+    Name = Column(String(50), unique=True, nullable=False)
+    Model = Column(String(50), nullable=False)
+    Type = Column(String(50))
+    PilotID = Column(Integer, ForeignKey(Character.CharacterID))
+    C = relationship(Character)
+
+class FilmData(Base):
+    __tablename__ = 'FilmData'
+    FilmDataID = Column(Integer, primary_key=True)
+    CharacterID = Column(Integer, ForeignKey(Character.CharacterID))
+    C = relationship(Character)
+    PlanetID = Column(Integer, ForeignKey(Planet.PlanetID))
+    P = relationship(Planet)
+    StarshipID = Column(Integer, ForeignKey(Starship.StarshipID))
+    S = relationship(Starship)
+    VehicleID = Column(Integer, ForeignKey(Vehicle.VehicleID))
+    V = relationship(Vehicle)
+
+class Film(Base):
+    __tablename__ = 'Film'
+    FilmID = Column(Integer, primary_key=True)
+    Title = Column(String(50), unique=True, nullable=False)
+    FilmDataID = Column(Integer, ForeignKey(FilmData.FilmDataID))
+    FD = relationship(FilmData)
+
+class Favorite(Base):
+    __tablename__ = 'Favorite'
+    FavoriteID = Column(Integer, primary_key=True)
+    CharacterID = Column(Integer, ForeignKey(Character.CharacterID))
+    C = relationship(Character)
+    PlanetID = Column(Integer, ForeignKey(Planet.PlanetID))
+    P = relationship(Planet)
+    StarshipID = Column(Integer, ForeignKey(Starship.StarshipID))
+    S = relationship(Starship)
+    VehicleID = Column(Integer, ForeignKey(Vehicle.VehicleID))
+    V = relationship(Vehicle)
+    FilmID = Column(Integer, ForeignKey(Film.FilmID))
+    F = relationship(Film)
+
 
     def to_dict(self):
         return {}
